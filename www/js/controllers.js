d519f316028af9061be2bc34d6b1e433687582e9
Miguel González Aravena <mgonzalez@produccion.datactil.com>
2016-01-18 09:31:17 -0300

    Agregar enlace y vista de información

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('app.controllers', [])
-.controller('miCompra', function($scope, $http) {
+.controller('miCompra', function($scope, $http, $ionicPopup) {
   $scope.valorTotal = 0;
   $scope.valorCompra = 1;
   $scope.comisionBanco = 2.5;
@@ -8,21 +8,74 @@ angular.module('app.controllers', [])
     return total * (($scope.comisionBanco / 100) + 1);
   } 
   $scope.valorEstimado = 0;
-  $scope.valorDolar = 690.32;
-  $scope.guardado = '2015-11-02';
+  $scope.valorDolar = 710.16;
+  $scope.guardado = '2015-12-31';
+  $scope.dolarCambiado = false;
   $scope.actualizarTotal = function() {
     $scope.valorTotal = $scope.factorComision((($scope.valorEstimado > 0 ? $scope.valorEstimado : $scope.valorDolar) + $scope.comisionDolar) * ($scope.valorCompra));
   }
+
+  var fecha = new Date() 
+    , mesActual = fecha.getMonth()
+    , anioActual = fecha.getFullYear()
+    , mesAnterior = mesActual
+    , anioAnterior = anioActual
+    , apiKey = '36fabbfba30a60a47700fe0a4fd91e005730b98a'
+  ;
+
   $http({
     method: 'GET'
-  , url: 'http://mindicador.cl/api'
+  , url: 'http://api.sbif.cl/api-sbifv3/recursos_api/dolar?apikey=' + apiKey + '&formato=json'
   })
   .then(function(response) {
-    $scope.valorDolar = response.data.dolar.valor;
-    $scope.actualizarTotal();
+    /* Si es que la petición envía un mensaje de error pero con código 201 */
+    if(typeof response.data.Mensaje != 'undefined') {
+      var confirmPopup = $ionicPopup.alert({
+        title: '¡Atención!',
+        template: responde.data.Mensaje,
+      });
+    } else {
+      $scope.valorDolar = parseFloat(response.data.Dolares[0].Valor.replace(',', '.'), 10);
+      $scope.dolarCambiado = true;
+      $scope.actualizarTotal();
+    }
   })
   .catch(function(err) {
+    /* En caso de que el día actual sea fin de semana o festivo, buscamos el último valor del dólar guardado */
+    if(mesActual == 0) {
+      mesAnterior = 11;
+      anioAnterior--;
+    } else {
+      mesAnterior--;
+    }
+    /* Seleccionamos el periodo a buscar (último mes) seleccionando el último valor guardado */
+    $http({
+      method: 'GET'
+    , url: 'http://api.sbif.cl/api-sbifv3/recursos_api/dolar/periodo/' + anioAnterior + '/' + (mesAnterior + 1) + '/' + anioActual + '/' + (mesActual + 1) + '?apikey=' + apiKey + '&formato=json'
+    })
+    .then(function(response) {
+      $scope.valorDolar = parseFloat(response.data.Dolares[response.data.Dolares.length-1].Valor.replace(',', '.'), 10);
+      $scope.dolarCambiado = true;
+      $scope.actualizarTotal();
+    })
+    /* En caso de que ambas peticiones fallen, mostramos el mensaje de error mostrado por la página del SBIF */
+    .catch(function(err) {
+      var confirmPopup = $ionicPopup.alert({
+        title: '¡Atención 2!',
+        template: err.data.Mensaje,
+      });
+    })
+    .finally(function() {
+      console.log($scope.dolarCambiado);
+      if(!$scope.dolarCambiado) {
+        var confirmPopup = $ionicPopup.alert({
+          title: '¡Atención 3!',
+          template: 'Se ha cargado el valor del dólar del día ' + $scope.guardado + ' (CLP $' + $scope.valorDolar + ').<br /><br /><b>Recuerda que se necesita una conexión Wi-Fi para poder acceder al valor del dólar actual.</b>',
+        });
+      }
+    });
   });
 
   $scope.actualizarTotal();
 })
+.controller('informacionCtrl', function($scope) {})
--- www/js/routes.js
+++ www/js/routes.js
@@ -8,126 +8,21 @@ angular.module('app.routes', [])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
-    .state('dIVERTRIP', {
-      url: '/page1',
-      templateUrl: 'templates/dIVERTRIP.html',
-      controller: 'dIVERTRIPCtrl'
-    })
-    
-    .state('inicio', {
-      url: '/page1',
-      templateUrl: 'templates/inicio.html',
-      controller: 'inicioCtrl'
-    })
-        
-    .state('menuPatrocinador', {
-      url: '/page2',
-      templateUrl: 'templates/menuPatrocinador.html',
-      controller: 'menuPatrocinadorCtrl'
-    })
-
-    .state('administrarEventos', {
-      url: '/page3',
-      templateUrl: 'templates/administrarEventos.html',
-      controller: 'administrarEventosCtrl'
-    })
-        
-    .state('contrasena', {
-      url: '/page4',
-      templateUrl: 'templates/contrasena.html',
-      controller: 'contrasenaCtrl'
-    })
-        
-    .state('historial', {
-      url: '/page5',
-      templateUrl: 'templates/historial.html',
-      controller: 'historialCtrl'
-    })
-
-    .state('reestablecer', {
-      url: '/page6',
-      templateUrl: 'templates/reestablecer.html',
-      controller: 'reestablecerCtrl'
-    })
-
-    .state('personas', {
-      url: '/page2',
-      templateUrl: 'templates/personas.html',
-      controller: 'personasCtrl'
-    })
-
-    .state('patrocinador', {
-      url: '/page3',
-      templateUrl: 'templates/patrocinador.html',
-      controller: 'patrocinadorCtrl'
-    })
-
-    .state('bienvenido', {
-      url: '/page4',
-      templateUrl: 'templates/bienvenido.html',
-      controller: 'bienvenidoCtrl'
-    })
-
-        
-    .state('menu', {
-      url: '/page5',
-      templateUrl: 'templates/menu.html',
-      controller: 'menuCtrl'
-    })
-        
-
-        
-    .state('eventos', {
-      url: '/page6',
-      templateUrl: 'templates/eventos.html',
-      controller: 'eventosCtrl'
-    })
-
-        
-    .state('mapa', {
-      url: '/page7',
-      templateUrl: 'templates/mapa.html',
-      controller: 'mapaCtrl'
-    })
-        
-
-        
-    .state('play', {
-      url: '/page8',
-      templateUrl: 'templates/play.html',
-      controller: 'playCtrl'
-    })
-
-        
-    .state('preferencias', {
-      url: '/page10',
-      templateUrl: 'templates/preferencias.html',
-      controller: 'preferenciasCtrl'
-    })
-        
-
-    .state('ayuda', {
-      url: '/page11',
-      templateUrl: 'templates/ayuda.html',
-      controller: 'ayudaCtrl'
-    })
-        
-
     .state('informacion', {
-      url: '/page12',
+      url: '/informacion',
       templateUrl: 'templates/informacion.html',
       controller: 'informacionCtrl'
     })
- 
-    .state('contacto', {
-      url: '/page13',
-      templateUrl: 'templates/contacto.html',
-      controller: 'contactoCtrl'
+    
+    .state('inicio', {
+      url: '/',
+      templateUrl: 'templates/inicio.html',
+      controller: 'miCompra'
     })
 
     ;
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/page1');
+  $urlRouterProvider.otherwise('/');
 
 });
